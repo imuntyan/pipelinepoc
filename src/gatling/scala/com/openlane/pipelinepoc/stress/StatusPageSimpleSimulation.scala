2bfa0dc835bbe6eca18e8d37b3cc11a20f3dbb2f
Igor Muntyan <Igor.Muntyan@openlane.com>
2016-11-24 16:05:26 -0500

    comments cleanup

--- src/gatling/scala/com/openlane/pipelinepoc/stress/StatusPageSimpleSimulation.scala
+++ src/gatling/scala/com/openlane/pipelinepoc/stress/StatusPageSimpleSimulation.scala
@@ -1,6 +1,6 @@
 package com.openlane.pipelinepoc.stress
 
-import io.gatling.core.Predef._ // 2
+import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 import com.openlane.pipelinepoc.stress.Constants._
@@ -9,18 +9,18 @@ class StatusPageSimpleSimulation extends Simulation {
 
   val url = sys.env(StatusPageUrlEnvVar)
 
-  val httpConf = http // 4
-    .baseURL(url) // 5
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+  val httpConf = http
+    .baseURL(url)
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("BasicSimulation") // 7
-    .exec(http("request_1")  // 8
-    .get("/status")) // 9
-    .pause(1) // 10
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+    .get("/status"))
+    .pause(1)
               
   setUp(scn.inject(atOnceUsers(1))).protocols(httpConf)
     .assertions(
--- src/gatling/scala/com/openlane/pipelinepoc/stress/StatusPageStressSimulation.scala
+++ src/gatling/scala/com/openlane/pipelinepoc/stress/StatusPageStressSimulation.scala
@@ -10,18 +10,18 @@ class StatusPageStressSimulation extends Simulation {
 
   val url = sys.env(StatusPageUrlEnvVar)
 
-  val httpConf = http // 4
-    .baseURL(url) // 5
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+  val httpConf = http
+    .baseURL(url)
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("BasicSimulation") // 7
-    .exec(http("request_1")  // 8
-    .get("/status")) // 9
-    .pause(1 second) // 10
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+    .get("/status"))
+    .pause(1 second)
               
   setUp(
     scn.inject(
